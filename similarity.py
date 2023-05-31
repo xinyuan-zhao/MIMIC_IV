60d4518e27414fb098d261f1dd0c5b283ad21094
xinyuan-zhao <xinyuan7@ualberta.ca>
2023-05-31 17:14:00 -0400

    add pic

--- similarity.py
+++ similarity.py
@@ -1,5 +1,12 @@
 import math
 import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+import os
+from networkx.algorithms import community
+import community
+from community import best_partition
+
 
 def similarity(patient1, patient2):
     '''
@@ -92,7 +99,70 @@ def create_minimum_spanning_tree(possible_edges_distance):
 
     return minimum_spanning_tree
 
+import networkx as nx
+import matplotlib.pyplot as plt
 
+def visualize_data(data):
+    # Create an empty graph
+    graph = nx.Graph()
+
+    # Add nodes to the graph
+    for row in data:
+        node1, node2, _ = row
+        graph.add_node(node1)
+        graph.add_node(node2)
+
+    # Add edges to the graph
+    for row in data:
+        node1, node2, _ = row
+        graph.add_edge(node1, node2)
+
+    # Visualize the graph
+    pos = nx.spring_layout(graph)  # Layout algorithm for node positioning
+    nx.draw_networkx(graph, pos=pos, with_labels=True, node_color='lightblue', node_size=500)
+    plt.axis('off')
+
+    # Increase the figure size to improve clarity
+    fig = plt.gcf()
+    fig.set_size_inches(12, 8)
+
+    script_dir = os.path.dirname(os.path.abspath(__file__))  # Get the directory where the script is located
+    save_path = os.path.join(script_dir, 'graph_visualization.png')  # Create the full file path
+
+    plt.savefig(save_path, dpi = 300)
+    plt.show()
+
+def visualize_data_Louvian(data):
+    # Create an empty graph
+    graph = nx.Graph()
+
+    # Add nodes to the graph
+    for row in data:
+        node1, node2, _ = row
+        graph.add_node(node1)
+        graph.add_node(node2)
+
+    # Add edges to the graph
+    for row in data:
+        node1, node2, _ = row
+        graph.add_edge(node1, node2)
+
+    # Perform community detection using Louvain algorithm
+    partition = community.best_partition(graph)
+
+    # Visualize the graph with communities highlighted
+    pos = nx.spring_layout(graph)
+    cmap = plt.get_cmap('viridis')
+    node_colors = [cmap(partition[node]) for node in graph.nodes()]
+
+    nx.draw_networkx(graph, pos=pos, with_labels=True, node_color=node_colors, node_size=500)
+    plt.axis('off')
+
+    # Increase the figure size to improve clarity
+    fig = plt.gcf()
+    fig.set_size_inches(12, 8)
+
+    plt.show()
 
 if __name__ == "__main__":
     # test functions using example from Alcaide et. al. 
@@ -114,13 +184,13 @@ if __name__ == "__main__":
                   similarity.values[0], 2)
 
     possible_edges_distance = convert_similarity_to_distance(possible_edges)
-    print(convert_similarity_to_distance(possible_edges))
+    # print(convert_similarity_to_distance(possible_edges))
 
     # create minimum spanning tree
     minimum_spanning_tree = create_minimum_spanning_tree(possible_edges_distance)
-    for edge in minimum_spanning_tree:
-        print(edge)
+    # for edge in minimum_spanning_tree:
+    #     print(edge)
 
-
-    print(f'Test 1: {test1} | passed: {test1==0.56}' )
-    print(f'Test 2: {test2} | passed: {test2==0.56}' )
+    visualize_data_Louvian(minimum_spanning_tree)
+    # print(f'Test 1: {test1} | passed: {test1==0.56}' )
+    # print(f'Test 2: {test2} | passed: {test2==0.56}' )
